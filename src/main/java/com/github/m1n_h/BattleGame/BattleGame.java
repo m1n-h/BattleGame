ffff314d4664a861491d3b8e5790ec9cc92d2e2d
m1n-h <khm2456@gmail.com>
2026-07-12 00:43:05 +0900

    refactor: 일반 몬스터 전투 밸런스 패치 및 포션 타겟팅 개선

--- src/main/java/com/github/m1n_h/BattleGame/BattleGame.java
+++ src/main/java/com/github/m1n_h/BattleGame/BattleGame.java
@@ -9,6 +9,9 @@ public class BattleGame {
         ArrayList<Hero> hero = new ArrayList<>();
         hero.add(new Warrior());
         hero.add(new Mage());
+        hero.add(new Archer());
+
+        int gold = 100;
 
         ArrayList<Monster> monster = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
@@ -18,53 +21,55 @@ public class BattleGame {
 
         System.out.println("GAME START! \uD83C\uDFB5\n");
         System.out.println("⚠\uFE0F 몬스터 연합군 " + monster.size() + "마리 출몰!!\n");
-        System.out.println("☄️ 전사 " + hero.get(0).getName() + "(이)가 대지진 주문을 외웁니다!\n");
-
-        int earthquakeDamage = 30;
 
         for (int i = 0; i < monster.size(); i++) {
             Monster target = monster.get(i);
-            target.takeDamage(earthquakeDamage);
 
-            if (target.getHp() <= 0) {
-                System.out.println("\uD83D\uDC80 " + target.getName() + " 은(는) 지진 충격을 이기지 못하고 쓰러졌습니다!\n");
-            } else {
-                System.out.println("💥 " + target.getName() + " 에게 " + earthquakeDamage + "의 피해! (남은 HP: " + target.getHp() + ")\n");
+            while (isPartyAlive(hero) && target.getHp() > 0) {
+                Hero victim = getRandomAliveHero(hero);
 
-                while (isPartyAlive(hero) && target.getHp() > 0) {
-                    Hero victim = getRandomAliveHero(hero);
-
-                    for (Hero party : hero) {
-                        if (target.getHp() > 0 && party.getHp() > 0) {
-                            party.attack(target);
-                            System.out.println();
-                        }
-                    }
-
-                    if (target.getHp() <= 0) {
-                        System.out.println("🎉 " + target.getName() + "이(가) 쓰러졌습니다!\n");
-
-                        if (victim != null) {
-                            victim.setHp(victim.getHp() + 50);
-                            System.out.println("🧪 포션 꿀꺽! " + victim.getName() + "의 HP가 회복되었습니다. (현재 HP: " + victim.getHp() + ")\n");
-                        }
-                        break;
-                    }
-
-
-                    if (target.getHp() > 0 && victim != null) {
-                        target.attack(victim);
-                        System.out.println("💥 " + victim.getName() + "이(가) " + target.getName() + "에게 공격당했습니다! (남은 HP: " + victim.getHp() + ")");
+                for (Hero party : hero) {
+                    if (target.getHp() > 0 && party.getHp() > 0) {
+                        party.attack(target);
                         System.out.println();
                     }
+                }
 
-                    if (!isPartyAlive(hero)) {
-                        System.out.println("💀 모든 용사가 쓰러졌습니다...\nYou Lose!\n");
-                        break;
+                if (target.getHp() <= 0) {
+                    System.out.println("🎉 " + target.getName() + " 이(가) 쓰러졌습니다!\n");
+
+                    Hero lowHpVictim = getLowestHpHero(hero);
+                    Hero lowMpVictim = getLowestMpHero(hero);
+
+                    if (victim != null) gold += 20;
+
+                    if (lowHpVictim != null) {
+                        lowHpVictim.setHp(lowHpVictim.getHp() + 50);
+                        System.out.println("🧪 포션 꿀꺽! " + lowHpVictim.getName() + " 의 HP가 회복되었습니다. (현재 HP: " + lowHpVictim.getHp() + ")");
                     }
+
+                    if (lowMpVictim != null) {
+                        lowMpVictim.setMp(lowMpVictim.getMp() + 100);
+                        System.out.println("🧪 포션 꿀꺽! " + lowMpVictim.getName() + " 의 MP가 회복되었습니다. (현재 MP: " + lowMpVictim.getMp() + ")");
+                    }
+                    break;
+                }
+
+
+                if (target.getHp() > 0 && victim != null) {
+                    target.attack(victim);
+                    System.out.println("💥 " + victim.getName() + " 이(가) " + target.getName() + " 에게 공격당했습니다! (남은 HP: " + victim.getHp() + ")");
+                    System.out.println();
+                }
+
+                if (!isPartyAlive(hero)) {
+                    System.out.println("💀 모든 용사가 쓰러졌습니다...\nYou Lose!\n");
+                    break;
                 }
             }
         }
+
+        if (isPartyAlive(hero)) gold = Shop.openShop(hero, gold);
 
 
         Scanner  sc = new Scanner(System.in);
@@ -72,8 +77,8 @@ public class BattleGame {
 
         KingSlime bossSlime = new KingSlime(bossWeak);
 
-        System.out.println("\uD83C\uDFB5 쿠구구궁... 최종 보스 " + bossSlime.getName() + "이(가) 나타났습니다! (HP: " + bossSlime.getHp() + ") \uD83C\uDFB5\\");
-        System.out.println(bossSlime.getName() + "의 약점 구역을 예측해 타격하세요!");
+        System.out.println("\uD83C\uDFB5 쿠구구궁... 최종 보스 " + bossSlime.getName() + " 이(가) 나타났습니다! (HP: " + bossSlime.getHp() + ") \uD83C\uDFB5\\");
+        System.out.println(bossSlime.getName() + " 의 약점 구역을 예측해 타격하세요!");
         System.out.println();
 
         while (bossSlime.getHp() > 0 && isPartyAlive(hero)) {
@@ -92,10 +97,10 @@ public class BattleGame {
                         System.out.println();
                     }
                 }
-                if (bossSlime.getHp() > 0) System.out.println("🦖 " + bossSlime.getName() + "의 남은 HP: " + bossSlime.getHp());
+                if (bossSlime.getHp() > 0) System.out.println("🦖 " + bossSlime.getName() + " 의 남은 HP: " + bossSlime.getHp());
 
             } else if (attackResult.equals("Miss")) {
-                System.out.println("🛡️ 팅! 공격이 단단한 외피에 막혔습니다. " + bossSlime.getName() + "의 남은 HP: " + bossSlime.getHp());
+                System.out.println("🛡️ 팅! 공격이 단단한 외피에 막혔습니다. " + bossSlime.getName() + " 의 남은 HP: " + bossSlime.getHp());
             }
 
             if (bossSlime.getHp() > 0) {
@@ -103,7 +108,7 @@ public class BattleGame {
 
                 if (bossVictim != null) {
                     System.out.println();
-                    System.out.println("🤢 " + bossSlime.getName() + "이 거대한 몸집으로 " + bossVictim.getName() + "을(를) 짓누릅니다! (남은 HP: " + bossVictim.getHp() + ")");
+                    System.out.println("🤢 " + bossSlime.getName() + "이 거대한 몸집으로 " + bossVictim.getName() + " 을(를) 짓누릅니다! (남은 HP: " + bossVictim.getHp() + ")");
 
                     bossVictim.takeDamage(bossSlime.getAttackPower());
                     System.out.println();
@@ -111,7 +116,7 @@ public class BattleGame {
             }
 
             if (bossSlime.getHp() <= 0) {
-                System.out.println("\n🎉 축하합니다! " + bossSlime.getName() + "을(를) 완전히 격파하고 세계를 구했습니다! 🏆");
+                System.out.println("\n🎉 축하합니다! " + bossSlime.getName() + " 을(를) 완전히 격파하고 세계를 구했습니다! 🏆");
                 break;
             }
 
@@ -141,5 +146,41 @@ public class BattleGame {
 
         int randomIdx = (int)  (Math.random() * aliveHero.size());
         return aliveHero.get(randomIdx);
+    }
+
+    private static Hero getLowestHpHero(ArrayList<Hero> party) {
+        ArrayList<Hero> aliveHero = new ArrayList<>();
+        for (Hero hero : party) {
+            if (hero.getHp() > 0) aliveHero.add(hero);
+        }
+        if (aliveHero.isEmpty())  return null;
+
+        Hero lowestHpHero = aliveHero.get(0);
+        for (int i = 1; i < aliveHero.size(); i++) {
+            Hero current = aliveHero.get(i);
+            if (current.getHp() < lowestHpHero.getHp()) {
+                lowestHpHero = current;
+            }
+        }
+
+        return lowestHpHero;
+    }
+
+    private static Hero getLowestMpHero(ArrayList<Hero> party) {
+        ArrayList<Hero> aliveHero = new ArrayList<>();
+        for (Hero hero : party) {
+            if (hero.getMp() > 0) aliveHero.add(hero);
+        }
+        if (aliveHero.isEmpty())  return null;
+
+        Hero lowestMpHero = aliveHero.get(0);
+        for (int i = 1; i < aliveHero.size(); i++) {
+            Hero current = aliveHero.get(i);
+            if (current.getMp() < lowestMpHero.getMp()) {
+                lowestMpHero = current;
+            }
+        }
+
+        return lowestMpHero;
     }
 }
