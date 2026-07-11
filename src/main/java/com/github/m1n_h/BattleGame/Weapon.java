445a759fae7fd9645dd2e8478dcbcadfb8054e1e
m1n-h <khm2456@gmail.com>
2026-07-12 00:44:42 +0900

    feat: 무기 공격력 강화 시스템 구현

--- src/main/java/com/github/m1n_h/BattleGame/Weapon.java
+++ src/main/java/com/github/m1n_h/BattleGame/Weapon.java
@@ -3,9 +3,15 @@ package com.github.m1n_h.BattleGame;
 public class Weapon {
     String name;
     int bonusAttack;
+    private int damage;
 
     public Weapon(String name, int bonusAttack) {
         this.name = name;
         this.bonusAttack = bonusAttack;
     }
+
+    public String getName() { return this.name; }
+
+    public int getDamage() { return this.damage; }
+    public void addDamage(int damage) { this.damage += damage; }
 }
